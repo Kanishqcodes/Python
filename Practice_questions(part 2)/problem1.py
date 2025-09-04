3934b4172fe92fd3a0150b1c46d383297ec57236
Kanishqcodes <kanishqfwp01@gmail.com>
2025-09-04 13:52:21 +0530

    Add files via upload

---
+++ Practice_questions(part 2)/problem1.py
@@ -0,0 +1,8 @@
+# Write a program to display a person's name,age and address in three diffrent lines
+Name= "Kanishq Panchal"
+Age= 20
+Address="Dwarkamai Society , Vikhroli (E)"
+
+print(Name)
+print(Age)
+print(Address)
